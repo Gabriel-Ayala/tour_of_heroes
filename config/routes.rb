f85e9e2b78564d505ae7a4a3f55eef4e52295931
Gabriel-Ayala <gabrielnevesayala@gmail.com>
2021-05-04 21:16:36 -0300

    add serializer and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :heroes
+  namespace :api, default: {format: :json} do
+    resources :heroes
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
